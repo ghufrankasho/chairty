1909b5955f920121d8fe768fb82e5c752d9fb5ce
Ghufran Kasho <ghufrankasho2@gmail.com>
2024-07-07 04:53:57 +0300

    commit

--- js/users/user.js
+++ js/users/user.js
@@ -1,14 +1,15 @@
 function displayusers() {
    
-    const xhr = new XMLHttpRequest();
+  const usersnum = document.getElementById('num_users');
+  const xhr = new XMLHttpRequest();
     xhr.open('GET', 'http://127.0.0.1:8000/api/user/', true);
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
                 const users = JSON.parse(xhr.responseText);
-               
+                usersnum.textContent=users.length;
                 const userContainer = document.getElementById('users');
-                const usernum = document.getElementById('num_employ');
+                
                  
                
                      
@@ -29,8 +30,13 @@ function displayusers() {
                         <td>${user.mobile}</td>
                         <td>${user.project_id}</td>
                         <td> <a href="#"><i class='bx bx-message-square-x' id="delete-${user.id}"></i></a>
-						<a href="#"><i class='bx bx-edit-alt' id="update-${user.id}"></i></a>
-						</td>`;
+                          <a href="#">  
+                          <svg xmlns="http://www.w3.org/2000/svg" id="view-${user.id}" width="20" height="20" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                          <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
+                          <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
+                          </svg>
+                          </a>
+                      </td>`;
 
                     
                 
@@ -39,20 +45,21 @@ function displayusers() {
                  
                  const deleteButton = document.getElementById(`delete-${user.id}`);
                     
-                 deleteButton.addEventListener('click',  () => {
+                deleteButton.addEventListener('click',  () => {
                    
                   if (window.confirm("هل متأكد من أنك تريد حذف هذا المستخدم؟")) { 
-                    deleteuser(user.id);}
+                    deleteuser(user.id);
+                  }
                    
                  }); 
-                //  const updateButton = document.getElementById(`update-${user.id}`);
+                const viewButton = document.getElementById(`view-${user.id}`);
                     
-                //  updateButton.addEventListener('click',  () => {
+                viewButton.addEventListener('click',  () => {
                
+                  window.location.href =`/users/viewUser.html?userId=${user.id}`;
+                  // viewuser(user.id);
                    
-                //    updateuser(user.id);
-                   
-                //  });
+                });
                 });
                 const searchButton = document.getElementById(`search`);
                     
@@ -77,7 +84,7 @@ function deleteuser(id) {
     
     
     const xhr = new XMLHttpRequest();
-    xhr.open('DELETE', `http://127.0.0.1:8000/api/user/delete/${id}`, true);
+    xhr.open('DELETE', `http://127.0.0.1:8000/api/user/delete?id=${id}`, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.onreadystatechange = function () {
        
@@ -97,7 +104,7 @@ function deleteuser(id) {
     };
     xhr.send();
     
-  }
+}
 function searchuser(input) {
   
   const data = JSON.stringify({ "search": input ,
