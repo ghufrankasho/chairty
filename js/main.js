cbd945531377d9d9b91bb50e8cb1dbaef189ade6
ramaalzain <ramaalzain21@gmail.com>
2024-07-15 03:52:44 +0300

    rama

--- js/main.js
+++ js/main.js
@@ -90,3 +90,47 @@ fetch("https://api.currencyfreaks.com/latest?apikey=631d974204e04c35afe1d8403bd9
     console.log(currency.rates["SAR"]);
     console.log(currency.rates["SYP"]);
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+var carouselItems = document.querySelectorAll('.landing-carousel-item');
+var carouselDots = document.querySelectorAll('.landing-carousel-dots>.dot');
+var currentItem = 0;
+
+function scrollToCarouselItem(i) {
+    carouselDots[currentItem].classList.remove('active');
+    carouselDots[i].classList.add('active');
+
+    document.getElementById('carousel-scroll-container').scrollTo({
+        left: window.innerWidth * i,
+        behavior: 'smooth'
+    })
+    currentItem = i;
+}
+
+carouselDots.forEach((dot, i) => {
+    dot.addEventListener('click', ev => {
+        ev.preventDefault();
+        scrollToCarouselItem(i);
+    })
+})
+
+setInterval(() => {
+    scrollToCarouselItem((currentItem + 1) % carouselItems.length)
+}, 5000);
